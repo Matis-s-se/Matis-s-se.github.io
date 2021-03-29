75338b5d5e405d5e3503299ad4c2eddedc2968e2
matis-s-se <matissedimier@gmail.com>
2021-03-29 13:38:20 +0200

    Avancement important

---
+++ main.js
@@ -0,0 +1,15 @@
+var variable1 = document.getElementById("passion");
+if(variable1 != null){
+  var egg = new Egg();
+  egg.addCode("p, a, s, s, i, o, n", function() {
+        window.location.href="http://technigres.com/";
+    }).listen();
+}
+
+var variable2 = document.getElementById("osu");
+if(variable2 != null){
+  var egg = new Egg();
+  egg.addCode("o, s, u", function() {
+    window.location.href="https://osu.ppy.sh/users/9910665";
+  }).listen();
+}
